6ab078e051eb3c3900b43911ece8a863b85e7b47
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 17:34:57 +0200

    html fix

--- js/script.js
+++ js/script.js
@@ -2,22 +2,20 @@
 for (let i = 1; i <= 100; i++) {
 
     // Variabile CONTENT
-    let content;
+    let content = i;
 
     if (i % 3 === 0 && i % 5 === 0) {
-        content ='FizzBuzz'; // Se sono multipli sia di 3 che di 5 
+        content = 'FizzBuzz'; // Se sono multipli sia di 3 che di 5 
 
     } else if(i % 3 === 0) {
-        content ='Fizz'; // Se ci sono mult. di 3 stampa Fizz
+        content = 'Fizz'; // Se ci sono mult. di 3 stampa Fizz
 
     } else if (i % 5 === 0) { // Se ci sono mult. di 5 stampa Buzz
-        content ='Buzz'; 
+        content = 'Buzz'; 
 
-    } else {
-        content = i; // Altrimenti stampa i 
     }
 
     // Stampiamo in console
-    console.log(content)
+    console.log(content);
 
 }
