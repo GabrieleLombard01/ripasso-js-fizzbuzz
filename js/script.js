b8bf2abfead8036cdc85f8d6f6f36984fd1663c5
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 17:11:56 +0200

    third step

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ for (let i = 1; i <= 100; i++) {
 
     if(i % 3 === 0) {
         console.log('Fizz') // Se ci sono mult. di 3 stampa Fizz
+
+    } else if (i % 5 === 0) { // Se ci sono mult. di 5 stampa Buzz
+        console.log('Buzz') 
+        
     } else {
         console.log(i); // Altrimenti stampa i 
     }
