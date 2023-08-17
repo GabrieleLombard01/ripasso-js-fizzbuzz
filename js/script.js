0ce8fc7fc0d930bd2ee32f93da2ec08c19a1fe85
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 17:04:05 +0200

    second step

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 // Ciclo FOR:
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    if(i % 3 === 0) {
+        console.log('Fizz') // Se ci sono mult. di 3 stampa Fizz
+    } else {
+        console.log(i); // Altrimenti stampa i 
+    }
+
 }
